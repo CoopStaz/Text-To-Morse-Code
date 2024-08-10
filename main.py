c60f60e560acfb96b49223d0977ad676f50d1e2b
CoopStaz <cjstaz13@gmail.com>
2024-08-10 22:46:11 +1000

    Create main functionality

--- main.py
+++ main.py
@@ -36,3 +36,17 @@ alphabet = {
     '9': '--- --- --- --- •',
     '0': '--- --- --- --- ---'
 }
+
+continue_play = 'yes'
+
+while continue_play == 'yes':
+    user_input = input('Type some text you wish to convert to morse code: ')
+    input_list_caps = list(user_input.upper())
+
+    morse_code_list = [alphabet[char] for char in input_list_caps]
+    morse_code = ' '.join(morse_code_list)
+
+    print(f'Original text: {user_input}\nMorse code: {morse_code}')
+
+    continue_play = input('Do you wish to convert some more text? (yes/no): ')
+
